ad75241622634be21aeedbef5aa41eb83e0874e9
Walid Hanif Ataullah <walidhnf12@gmail.com>
2023-04-09 12:47:09 +0700

    first commit

---
+++ tpmodul8_1302213120/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302213120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
